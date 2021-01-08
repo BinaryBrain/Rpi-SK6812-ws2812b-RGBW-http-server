0bd1c755d675ed30eb0be7cb676e3e20ad1a7227
Sacha Bron <me@sachabron.ch>
2021-01-08 21:11:51 +0100

    Move ledManager to its own file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const ws281x = require('rpi-ws281x');
+
 const dgram = require('dgram');
 const express = require('express');
-const ws281x = require('rpi-ws281x');
 const bodyParser = require('body-parser');
+
+const LedManager = require('ledManager.js');
+const ledManager = new LedManager();
 
 const LED_NB = ledNb(300);
 const PIN = 18;
-
-const ledManager = new LedManager();
 
 function runUdpServer() {
     const server = dgram.createSocket('udp4');
@@ -83,71 +85,6 @@ function ledNb(nleds) {
         return nbytes / 3;
     }
 }
-
-class LedManager {
-    constructor() {
-        this.config = {};
-        this.config.leds = LED_NB;
-        this.config.brightness = 255;
-        this.config.gpio = PIN;
-        this.config.strip = 'rgb';
-        ws281x.configure(this.config);
-    }
-
-    // FORMAT: ["FFAA66", "FFAA66FF"]
-    setColorsStr (colors) {
-        const colorArray = [];
-
-        for (let color of colors) {
-            const value = parseInt(color, 16);
-
-            let r = 0;
-            let g = 0;
-            let b = 0;
-            let w = 0;
-
-            if (color.length == 8) {
-                r = (value >> 24) & 255;
-                g = (value >> 16) & 255;
-                b = (value >> 8) & 255;
-                w = value & 255;
-            } else {
-                r = (value >> 16) & 255;
-                g = (value >> 8) & 255;
-                b = value & 255;
-                w = 0;
-            }
-
-            colorArray.push(r, g, b, w);
-        }
-
-        const pixels = translate(colorArray);
-
-        // Render to strip
-        ws281x.render(pixels);
-    }
-
-    // FORMAT: [{r: 255, g: 255, b: 255, w?: 255}]
-    setColorsObj (colors) {
-        const colorArray = [];
-
-        for (let color of colors) {
-            if (!color.w) {
-                color.w = 0;
-            }
-
-            colorArray.push(color.w); // White
-            colorArray.push(color.g); // Green
-            colorArray.push(color.r); // Red
-            colorArray.push(color.b); // Blue
-        }
-
-        const pixels = translate(colorArray);
-
-        // Render to strip
-        ws281x.render(pixels);
-    }
-};
 
 function translate(a) {
     const newArray = new Uint32Array(LED_NB);
---
+++ ledManager.js
@@ -0,0 +1,68 @@
+const ws281x = require('rpi-ws281x');
+
+class LedManager {
+    constructor() {
+        this.config = {};
+        this.config.leds = LED_NB;
+        this.config.brightness = 255;
+        this.config.gpio = PIN;
+        this.config.strip = 'rgb';
+        ws281x.configure(this.config);
+    }
+
+    // FORMAT: ["FFAA66", "FFAA66FF"]
+    setColorsStr (colors) {
+        const colorArray = [];
+
+        for (let color of colors) {
+            const value = parseInt(color, 16);
+
+            let r = 0;
+            let g = 0;
+            let b = 0;
+            let w = 0;
+
+            if (color.length == 8) {
+                r = (value >> 24) & 255;
+                g = (value >> 16) & 255;
+                b = (value >> 8) & 255;
+                w = value & 255;
+            } else {
+                r = (value >> 16) & 255;
+                g = (value >> 8) & 255;
+                b = value & 255;
+                w = 0;
+            }
+
+            colorArray.push(r, g, b, w);
+        }
+
+        const pixels = translate(colorArray);
+
+        // Render to strip
+        ws281x.render(pixels);
+    }
+
+    // FORMAT: [{r: 255, g: 255, b: 255, w?: 255}]
+    setColorsObj (colors) {
+        const colorArray = [];
+
+        for (let color of colors) {
+            if (!color.w) {
+                color.w = 0;
+            }
+
+            colorArray.push(color.w); // White
+            colorArray.push(color.g); // Green
+            colorArray.push(color.r); // Red
+            colorArray.push(color.b); // Blue
+        }
+
+        const pixels = translate(colorArray);
+
+        // Render to strip
+        ws281x.render(pixels);
+    }
+};
+
+module.exports = LedManager;
